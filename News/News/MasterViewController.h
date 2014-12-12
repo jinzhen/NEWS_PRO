7ec53d4e1b8b34362aabeb48bc863178d3c4c1cd
HuangLei <huangleililei@gmail.com>
2014-12-12 15:49:09 +0800

    create new project

---
+++ News/News/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  News
+//
+//  Created by lihaijin on 14-12-12.
+//  Copyright (c) 2014年 lihaijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
---
+++ News/News/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  News
+//
+//  Created by lihaijin on 14-12-12.
+//  Copyright (c) 2014年 lihaijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
